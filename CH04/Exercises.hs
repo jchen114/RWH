a5d0cbce2f62ad73b94375402f4ea2ffa9b24e08
Jacob Chen <chen.jacob92@gmail.com>
2015-02-18 00:37:14 -0500

    Chapter 4 Exercises

--- CH04/Exercises.hs
+++ CH04/Exercises.hs
@@ -1,4 +1,8 @@
+import Data.List
+import Data.Char
 
+type ErrorMessage = String
+  
 safeHead' :: [a] -> Maybe a
 safeHead' [] = Nothing
 safeHead' (a:as) = Just a
@@ -20,5 +24,40 @@ splitWith _ [] = [[]]
 splitWith f as = front : splitWith f (dropWhile f back)
   where (front, back) = break f as 
 
---transpose :: String -> String
---transpose str = lines str
+transpose' :: String -> String
+transpose' str = (intercalate "\n" . transpose . lines) str
+
+asInt_fold :: String -> Int
+asInt_fold str
+  | head str == '-' = (-1) * foldr digify 0 (reverse $ tail str)
+  | otherwise = foldr digify 0 (reverse str)
+  where digify x a = a*10 + digitToInt x
+
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either xs
+  | (head xs) `notElem` validChars = Left ("non-digit '" ++ [head xs] ++ "'" )
+  | otherwise = Right (asInt_fold xs)
+  where validChars = ['0'..'9'] ++ ['-']
+
+concat' :: [[a]] -> [a]
+concat' xss = foldr (++) [] xss
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' f (a:as)
+  | f a = [a] ++ takeWhile' f as
+  | otherwise = []
+                
+takeWhile'' :: (a -> Bool) -> [a] -> [a]
+takeWhile'' f xs = foldr h [] xs
+  where h x y
+          | f x = [x] ++ y
+          | otherwise = []
+
+groupBy' :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy' _ [] = [[]]
+groupBy' f (x:xs) = foldl h [[x]] xs
+  where h x y
+          | f ((head . last) x) y = init x ++ [last x ++ [y]]
+          | otherwise = x ++ [[y]]
+        
